e442a30025984a208c17771db55d02b18bafa0e0
Aurélien Bertron <aure_bertron@hotmail.com>
2012-08-25 19:01:23 +0200

    re-versionne config.php à cause de soucis

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+	Base de donnees
+*/
+
+define("DBSERVER", "localhost");
+define("DBNAME", "etudiants");
+define("DBUSER", "root");
+define("DBPASSWD", "root");
+
+/*
+	Chemins des repertoires
+*/
+
+define("PATH_ROOT", $_SERVER["SERVER_NAME"].":8888/etudiantsIUT");
+define("PATH_MODULES", "./modules");
+define("PATH_MODELES", "./modeles");
+define("PATH_TEMPLATES", "./templates");
